c13d5e1eea5f29a04fa04e5e53b4f27fe8c6263d
tomkita <tomkita00@gmail.com>
2012-06-15 11:52:44 -0500

    Changed class name

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-include_once('curl_request.php');
+include_once('hyper_table.php');
 
-$curl = new curl(array('url' => 'path/to/url'));
+$curl = new hyper_table(array('url' => 'path/to/url'));
 $result = $curl->init(array(
     'key_1' => 'value_1',
     'key_2' => 'value_2' 
